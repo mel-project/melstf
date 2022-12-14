57aafbbdf8db62b803227f965e6ef1c5cff80e6b
nullchinchilla <nullchinchilla@pm.me>
2022-12-14 12:38:33 -0600

    tip911 data structure

---
+++ lib/tip911-stakeset/src/lib.rs
@@ -0,0 +1,94 @@
+use novasmt::{dense::DenseMerkleTree, ContentAddrStore, Database, InMemoryCas};
+use stdcode::StdcodeSerializeExt;
+use themelio_structs::{CoinID, CoinValue, StakeDoc, TxHash};
+use tmelcrypt::{Ed25519PK, Hashable};
+
+/// Keeps track of all the active stakes in the blockchain. Abstracts over the pre-TIP911 and post-TIP911 representations.
+pub struct StakeSet {
+    stakes: imbl::HashMap<TxHash, StakeDoc>,
+}
+
+impl StakeSet {
+    /// Adds another stake to the set. The transaction hash of the staking transaction, as well as the parsed [StakeDoc], must be given.
+    pub fn add_stake(&mut self, txhash: TxHash, stake: StakeDoc) {
+        self.stakes.insert(txhash, stake);
+    }
+
+    /// Checks whether a particular CoinID is frozen due to it containing an active stake.
+    pub fn is_frozen(&self, coin: CoinID) -> bool {
+        self.stakes.contains_key(&coin.txhash)
+    }
+
+    /// Checks how many votes a particular staker has, in the given epoch.
+    pub fn votes(&self, epoch: u64, key: Ed25519PK) -> u128 {
+        self.stakes
+            .values()
+            .filter(|v| v.e_start <= epoch && v.e_post_end > epoch && v.pubkey == key)
+            .map(|v| v.syms_staked.0)
+            .sum()
+    }
+
+    /// Obtains the number of votes in total for the given epoch.
+    pub fn total_votes(&self, epoch: u64) -> u128 {
+        self.stakes
+            .values()
+            .filter(|v| v.e_start <= epoch && v.e_post_end > epoch)
+            .map(|v| v.syms_staked.0)
+            .sum()
+    }
+
+    /// Removes all the stakes that have expired by this epoch.
+    pub fn unlock_old(&mut self, epoch: u64) {
+        self.stakes.retain(|_, v| v.e_post_end >= epoch);
+    }
+
+    /// Obtain an old-style (pre-TIP-911) sparse merkle tree that maps the **hash of the stdcode encoding of** the transaction hash of the transaction that created the stake, to the stdcode-encoded stake.
+    pub fn pre_tip911(&self) -> novasmt::Tree<InMemoryCas> {
+        let mut tree = Database::new(InMemoryCas::default())
+            .get_tree([0u8; 32])
+            .unwrap();
+        for (k, v) in self.stakes.iter() {
+            tree.insert(k.stdcode().hash().0, &v.stdcode());
+        }
+        tree
+    }
+
+    /// Obtain a TIP-911-style object.
+    pub fn post_tip911(&self, epoch: u64) -> Tip911 {
+        let mut stakes: Vec<(TxHash, StakeDoc)> = self.stakes.clone().into_iter().collect();
+        // sort by txhash
+        stakes.sort_unstable_by_key(|s| s.0);
+        // then, sort *stably* by stake size.
+        stakes.sort_by_key(|s| s.1.syms_staked);
+        Tip911 {
+            current_total: self.total_votes(epoch).into(),
+            next_total: self.total_votes(epoch + 1).into(),
+            stakes,
+        }
+    }
+}
+
+pub struct Tip911 {
+    /// Tally of all the SYM that can vote in this epoch.
+    pub current_total: CoinValue,
+    /// Tally of all the SYM that can vote in the next epoch.
+    pub next_total: CoinValue,
+    /// Vector of all the stakedocs, sorted first by stake size, and then by txhash.
+    pub stakes: Vec<(TxHash, StakeDoc)>,
+}
+
+impl Tip911 {
+    /// Calculates a dense merkle tree where each element is the hash of the stdcode of (current_total, next_total, txhash, stakedoc).
+    pub fn calculate_merkle(&self) -> DenseMerkleTree {
+        let vec = self
+            .stakes
+            .iter()
+            .map(|(txhash, sdoc)| {
+                (self.current_total, self.next_total, txhash, sdoc)
+                    .stdcode()
+                    .hash()
+            })
+            .collect::<Vec<_>>();
+        DenseMerkleTree::new(&vec)
+    }
+}
